55471a13ddd086e15ac37b5fe50957b8f5b4984f
rudi <rudi>
2001-12-29 10:56:18 +0000

    * Commented out loading of aserve:example.cl
    * Sanitized cmucl build/load process: loading of gray-streams with/without
      common-lisp-controller, init multiprocessing

--- INSTALL.lisp
+++ INSTALL.lisp
@@ -4,85 +4,96 @@
 
 #+lispworks
 (progn
-; Load logical host definitions
-(load (merge-pathnames "logical-hostnames.lisp" *load-truename*))
+  ;; Load logical host definitions
+  (load (merge-pathnames "logical-hostnames.lisp" *load-truename*))
 
-; Load definition of CL-SSL system
-;(load "cl-ssl:defsys.lisp")
+  ;; Load definition of CL-SSL system
+  ;;(load "cl-ssl:defsys.lisp")
 
-; Compile and load CL-SSL system
-;(scm:compile-system 'cl-ssl :load t) ;use :FORCE T if compilation troubles
+  ;; Compile and load CL-SSL system
+  ;;(scm:compile-system 'cl-ssl :load t) ;use :FORCE T if compilation troubles
 
-; Load definition of ACL Compatibility system
-(load "acl-compat:defsys.lisp")
+  ;; Load definition of ACL Compatibility system
+  (load "acl-compat:defsys.lisp")
 
-; Compile and load ACL Compatibility system
-(scm:compile-system 'acl-compat :load t :force t) ;use :FORCE T if compilation troubles
+  ;; Compile and load ACL Compatibility system
+  (scm:compile-system 'acl-compat :load t :force t) ;use :FORCE T if compilation troubles
 
-; Load definition of Aserve system
-(load "aserve:defsys.lisp")
+  ;; Load definition of Aserve system
+  (load "aserve:defsys.lisp")
 
-; Compile and load the ASERVE system
-(scm:compile-system 'aserve :load t :force t) ;use :FORCE T if compilation troubles
+  ;; Compile and load the ASERVE system
+  (scm:compile-system 'aserve :load t :force t) ;use :FORCE T if compilation troubles
 )
 
+
 #+cmu
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (progn
-;; We need gray-streams in the image.  The following works with Debian
-;; packages of cmucl 2.5.1 + cmucl-source + common-lisp-controller.
-;; Adapt as necessary.
+(progn
+  ;; Load Gray-streams support
+  (eval-when (:compile-toplevel :load-toplevel :execute)
+    ;; Debian cmucl distribution
+    #+(and common-lisp-controller (not gray-streams))
+    (require :cmucl-graystream)
+    ;; vanilla cmucl
+    #+(and (not common-lisp-controller) (not gray-streams))
+    (progn (load "library:subsystems/gray-streams-library")
+           (pushnew :gray-streams *features*)))
 
-(unless (member :gray-streams *features*)
-  (mk:oos :cmucl-graystream :load))
+  ;; Startup multiprocessing.
+  ;;
+  ;; this isn't strictly necessary, but scheduling feels very coarse
+  ;; before the evaluation of (startup-idle-and-top-level-loops) --
+  ;; answer delays of about 1s per http request.
+  ;;
+  ;; KLUDGE: startup-idle-and-top-level-loops can only be evaluated
+  ;; once, so we look for something resembling an existing idle loop
+  ;; before invoking it.
+  #||
+  #+mp
+  (unless (find-if
+           #'(lambda (proc) (string= (mp:process-name proc) "Idle Loop"))
+           (mp:all-processes))
+    (mp::startup-idle-and-top-level-loops))
+  ||#
+  ;; DOUBLE KLUDGE: The preceding (commented-out) form caused the
+  ;; loading of INSTALL.lisp to abort silently (!), so we do the
+  ;; following, pilfered from eclipse the window manager:
+  #+mp
+  (setf mp::*idle-process* mp::*initial-process*)
 
-;; this isn't strictly necessary, but scheduling feels very coarse
-;; without startup-idle-and-top-level-loops, leading to answer delays
-;; of about 1s per request.
+  ;; Load logical host definitions
+  (load (merge-pathnames "logical-hostnames.lisp" *load-truename*))
 
-(unless (find-if
-         #'(lambda (proc) (string= (mp:process-name proc) "Top Level Loop"))
-         (mp:all-processes))
-  (mp::startup-idle-and-top-level-loops))
+  ;; Load definition of ACL Compatibility system
+  (load "acl-compat:acl-compat.system")
 
+  ;; Compile and load ACL Compatibility system
+  (mk:oos "ACL-COMPAT" :load :compile-during-load t)
 
-; Load logical host definitions
-(load (merge-pathnames "logical-hostnames.lisp" *load-truename*))
+  ;; Load definition of Aserve system
+  (load "aserve:aserve.system")
 
-;(load "/home/jsc/work/src/cl-ssl/logical-hostnames.lisp")
-;(load "cl-ssl:cl-ssl.system")
-;(mk:oos :cl-ssl :compile)
-;(pushnew :cl-ssl *features*)
+  ;; Compile and load the ASERVE system
+  (mk:oos "ASERVE" :load :compile-during-load t)
+  
+  )
 
-; Load definition of ACL Compatibility system
-(load "acl-compat:acl-compat.system")
+#||
+;;; To test the installation, evaluate the following:
 
-; Compile and load ACL Compatibility system
-(mk:oos "ACL-COMPAT" :load :compile-during-load t)
+;;; Load example.lisp in the aserve directory.
+(load "aserve:example.cl")
 
-; Load definition of Aserve system
-(load "aserve:aserve.system")
-
-; Compile and load the ASERVE system
-(mk:oos "ASERVE" :load :compile-during-load t)
-))
-
-
-; Test the installation
-
-; Load example.lisp in the aserve directory.
-(load "aserve:example.lisp")
-
-; Select example package
+;;; Select example package
 (in-package :aserve-example)
 
-;; This option enables extended debug message output
+;;; This option enables extended debug message output
 (net.aserve::debug-on :info)
 
-;; This option enables to enter the debugger if an
-;; error occurs. (instead of simply logging and ignoring it)
-
+;;; This option enables to enter the debugger if an error
+;;; occurs. (instead of simply logging and ignoring it)
 (net.aserve::debug-on :notrap)
 
-; Start example server (in multiprocessing) on port 2001
-;(start-server :port 2001)
+;;; Start example server (in multiprocessing) on port 2001
+(start-server :port 2001)
+||#
