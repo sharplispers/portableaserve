f232441efe0195e547c281de9ef01b4aafb7b355
desoi <desoi>
2002-04-08 14:26:49 +0000

    Initial version for MCL and OpenMCL.

---
+++ acl-compat/acl-sys-mcl.lisp
@@ -0,0 +1,30 @@
+
+
+(defpackage system
+  (:nicknames :sys)
+  (:use :common-lisp) 
+  (:export
+   "command-line-arguments"
+   "command-line-argument"
+   "reap-os-subprocess"
+   ))
+
+(in-package :system)
+
+
+; none of this stuff now; maybe with OS X
+
+(defun command-line-arguments ()
+  nil)
+  ;system:*line-arguments-list*)
+
+(defun command-line-argument (n)
+  (declare (ignore n))
+  nil)
+  ;(nth n system:*line-arguments-list*))
+
+(defun reap-os-subprocess (&key (wait nil))
+  (declare (ignore wait))
+  nil)
+
+(export '(command-line-arguments command-line-argument reap-os-subprocess))
