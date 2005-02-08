08bdab31e87c805e9e44a13a694e5f249c8c6a06
rudi <rudi>
2005-02-08 09:55:43 +0000

    * Move string-to-octets from acl-excl-common.lisp into *-excl.lisp,   since sbcl has a native version.

--- acl-compat/scl/acl-excl.lisp
+++ acl-compat/scl/acl-excl.lisp
@@ -229,5 +229,36 @@
 
 ;;; End bivalent Gray streams
 
+(defun string-to-octets (string &key (null-terminate t) (start 0)
+                         end mb-vector make-mb-vector?
+                         (external-format :default))
+  "This function returns a lisp-usb8-vector and the number of bytes copied."
+  (declare (ignore external-format))
+  ;; The end parameter is different in ACL's lambda list, but this
+  ;; variant lets us give an argument :end nil explicitly, and the
+  ;; right thing will happen
+  (unless end (setf end (length string)))
+  (let* ((number-of-octets (if null-terminate (1+ (- end start))
+                               (- end start)))
+         (mb-vector (cond
+                      ((and mb-vector (>= (length mb-vector) number-of-octets))
+                       mb-vector)
+                      ((or (not mb-vector) make-mb-vector?)
+                       (make-array (list number-of-octets)
+                                   :element-type '(unsigned-byte 8)
+                                   :initial-element 0))
+                      (t (error "Was given a vector of length ~A, ~
+                                 but needed at least length ~A."
+                                (length mb-vector) number-of-octets)))))
+    (declare (type (simple-array (unsigned-byte 8) (*)) mb-vector))
+    (loop for from-index from start below end
+       for to-index upfrom 0
+       do (progn
+            (setf (aref mb-vector to-index)
+                  (char-code (aref string from-index)))))
+    (when null-terminate
+      (setf (aref mb-vector (1- number-of-octets)) 0))
+    (values mb-vector number-of-octets)))
+
 
 (provide 'acl-excl)
